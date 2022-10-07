9d30cc3392702ea6ac3ce02cba9cbeb7b116594a
snorq2 <81600811+snorq2@users.noreply.github.com>
2022-10-06 22:45:39 -0700

    Got matrix split and initial calculations working

--- hw1.py
+++ hw1.py
@@ -1,11 +1,22 @@
 import numpy as np
+from numpy import genfromtxt
 
 new_matrix = np.array([[1, 2, 3], [2, 4, 6], [2, 7, 8]])
 print(new_matrix)
 
+mnist_train = genfromtxt("mnist_train.csv", dtype=float, delimiter=',')
+mnist_test = genfromtxt("mnist_test.csv", dtype=float, delimiter=',')
 
-def helloPrint():
-    print("Hello world")
+# Copies data into new array while dividing into required range
+mnist_train_data = mnist_train[:, range(1, len(mnist_train[0]))]/255
+
+# copies training values into new array
+mnist_train_values = mnist_train[:, 0]
 
 
-helloPrint()
+# for i in range(1,len(mnist_train))
+print(len(mnist_train))
+print(len(mnist_test))
+
+
+print('done loading')
